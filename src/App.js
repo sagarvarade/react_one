67de50479d4fefa2981482db114119bd1725d2a6
APME-OFFICE\svarade <svarade@euronetworldwide.com>
2023-10-21 13:44:08 +0530

    Dd

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from "react";
 //import Simple from "./Components/Simple/Simple"
 //import Pizza from "./Components/Pizza/Pizza";
 //import InterActive from "./Components/Steps/Steps";
-import TravelList from "./Components/TravelList/TravelListComp";
+//import TravelList from "./Components/TravelList/TravelListComp";
+import EatAndSplit from "./Components/EatAndSplit/EatAndSplit";
 function App() {
   return (
     <>
       {/* <Simple /> */}
       {/* <Pizza /> */}
       {/* <InterActive /> */}
-      <TravelList />
+      {/* <TravelList /> */}
+      <EatAndSplit />
       </>
   );
 }
